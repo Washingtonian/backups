546f2ed74a14dcce8cf01fd9b819b151acae70e5
Tom Shafer <tj@tjshafer.com>
2017-10-23 11:45:28 -0700

    cleanup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,13 +66,10 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-
-
             <div class="content">
                 <div class="title m-b-md">
                     Silence is Bliss
                 </div>
-
             </div>
         </div>
     </body>
